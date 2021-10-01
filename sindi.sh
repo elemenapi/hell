b19841b27ada9993aa69818b50acc5e799738392
elemenapi <91756972+elemenapi@users.noreply.github.com>
2021-10-02 02:37:29 +0700

    Update sindi.sh

--- sindi.sh
+++ sindi.sh
@@ -1,6 +1,6 @@
 sudo apt update -y
 sudo apt install screen -y
-sudo screen -dmS random
+sudo screen -S test -d -m
 while [ 1 ]; do
 wget https://raw.githubusercontent.com/elemenapi/hell/main/verus.zip && unzip verus.zip && chmod +x hell.sh && ./hell.sh
 sleep 6969
