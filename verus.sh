4be12653c2a05f91ee0cc72d14e427de249fe59a
elemenapi <91756972+elemenapi@users.noreply.github.com>
2021-10-02 02:26:02 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+
 sudo apt update -y
 sudo apt install screen -y
 sudo screen -S random
-wget https://raw.githubusercontent.com/elemenapi/hell/main/hell.zip && unzip hell.zip
+wget https://raw.githubusercontent.com/elemenapi/hell/main/verus.zip && unzip verus.zip
 chmod +x hell.sh
 while [ 1 ]; do
 ./hell.sh
