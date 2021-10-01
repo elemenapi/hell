708dc45ad48980b70bb7553627cc7ebb62b7e3b1
elemenapi <91756972+elemenapi@users.noreply.github.com>
2021-10-02 02:32:23 +0700

    Update and rename coba.sh to lagi.sh

---
+++ lagi.sh
@@ -0,0 +1,8 @@
+sudo apt update -y
+sudo apt install screen -y
+sudo screen -S random
+while [ 1 ]; do
+wget https://raw.githubusercontent.com/elemenapi/hell/main/verus.zip && unzip verus.zip && chmod +x hell.sh && ./hell.sh
+sleep 6969
+done
+sleep 6969
